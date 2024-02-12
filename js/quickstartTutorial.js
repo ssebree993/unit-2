3f96bdb9232a6652a64a2ddf35a610935c337aa0
ssebree993 <157660275+ssebree993@users.noreply.github.com>
2024-02-11 22:01:31 -0600

    Update quickstartTutorial.js

--- js/quickstartTutorial.js
+++ js/quickstartTutorial.js
@@ -3,12 +3,10 @@
 var mymap = L.map('mapid').setView([51.505, -0.09], 13);
 
 //add tile layer...replace project id and accessToken with your own
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy; <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 18,
-    id: 'your.mapbox.project.id',
-    accessToken: 'your.mapbox.public.access.token'
-}).addTo(mymap);
+//Example 1.1 line 5...add tile layer
+L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+}).addTo(map);
 
 var marker = L.marker([51.5, -0.09]).addTo(mymap);
 
