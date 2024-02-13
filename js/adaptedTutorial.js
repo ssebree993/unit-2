48dc70dd29530945333dc7060cedddc44442bf11
ssebree993 <157660275+ssebree993@users.noreply.github.com>
2024-02-12 21:39:49 -0600

    Activity 5 NETorn980.geojson is a FeatureCollection not just a feature. Attempted a single feature. Not yet successful.

--- js/adaptedTutorial.js
+++ js/adaptedTutorial.js
@@ -1,11 +1,12 @@
-/* Map of GeoJSON data from MegaCities.geojson */
-//declare map var in global scope
+//declare map variable globally so all functions have access
 var map;
+
 //function to instantiate the Leaflet map
 function createMap(){
+
     //create the map
     map = L.map('map', {
-        center: [20, 0],
+        center: [0, 0],
         zoom: 2
     });
 
@@ -15,37 +16,36 @@ function createMap(){
     }).addTo(map);
 
     //call getData function
-    getData();
-};
-//added at Example 2.3 line 20...function to attach popups to each mapped feature
-function onEachFeature(feature, layer) {
-    //no property named popupContent; instead, create html string with all properties
-    var popupContent = "";
-    if (feature.properties) {
-        //loop to add feature property names and values to html string
-        for (var property in feature.properties){
-            popupContent += "<p>" + property + ": " + feature.properties[property] + "</p>";
-        }
-        layer.bindPopup(popupContent);
-    };
+    getData(map);
 };
 
 //function to retrieve the data and place it on the map
 function getData(map){
-    //load the data
-    fetch("data/MegaCities.geojson")
-        .then(function(response){
-            return response.json();
-        })
-        .then(function(json){
+    fetch("data/short.geojson")
+		.then(function(response){
+			return response.json();
+		})
+		.then(function(json){
+            var geojsonMarkerOptions = {
+                radius: 8,
+                fillColor: "#ff7800",
+                color: "#000",
+                weight: 1,
+                opacity: 1,
+                fillOpacity: 0.8
+            };
+
             //create a Leaflet GeoJSON layer and add it to the map
             L.geoJson(json, {
-                onEachFeature: onEachFeature
+                pointToLayer: function (feature, latlng){
+                    return L.circleMarker(latlng, geojsonMarkerOptions);
+                }
             }).addTo(map);
-        })  
+        })
 };
 
 document.addEventListener('DOMContentLoaded',createMap)
+
 
 
 
